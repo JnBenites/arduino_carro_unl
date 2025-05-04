a6fc38cc639fe2e0a442d3e4e4be771178bf2365
Juan B <55644440+JnBenites@users.noreply.github.com>
2025-05-04 10:50:24 -0500

    Update codigo_arduino.ino

--- codigo_arduino.ino
+++ codigo_arduino.ino
@@ -1,26 +1,27 @@
 // Pines de los motores
 const int IN1 = 9;  // Motor izquierdo (IN1)
 const int IN2 = 8;  // Motor izquierdo (IN2)
-const int IN3 = 7;  // Motor derecho (IN3)
-const int IN4 = 6;  // Motor derecho (IN4)
+const int IN3 = 7;  // Motor derecho  (IN3)
+const int IN4 = 6;  // Motor derecho  (IN4)
 
-// Tiempo de movimiento en milisegundos
-const int TIEMPO_MOVIMIENTO = 900;
+// Tiempos en milisegundos
+const unsigned long TIEMPO_MOVIMIENTO = 500;  // adelante / atrás
+const unsigned long TIEMPO_GIRO      = 100;  // giro izquierda / derecha
 
-unsigned long tiempoInicio = 0;
-bool enMovimiento = false;
-char comandoActual = '\0';
+unsigned long tiempoInicio     = 0;
+unsigned long duracionActual  = 0;
+bool enMovimiento             = false;
+char comandoActual            = '\0';
 
 void setup() {
   Serial.begin(9600);  // Inicia comunicación Bluetooth
 
-  // Configura pines de los motores
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  detenerMotores();  // Inicia con motores detenidos
+  detenerMotores();
 }
 
 void loop() {
@@ -28,18 +29,18 @@ void loop() {
   if (Serial.available() && !enMovimiento) {
     comandoActual = Serial.read();
     iniciarMovimiento(comandoActual);
-    tiempoInicio = millis();
-    enMovimiento = true;
+    tiempoInicio    = millis();
+    enMovimiento    = true;
   }
 
-  // Si ya pasó 1 segundo desde que se inició el movimiento
-  if (enMovimiento && millis() - tiempoInicio >= TIEMPO_MOVIMIENTO) {
+  // Comprueba si ya pasó la duración configurada
+  if (enMovimiento && millis() - tiempoInicio >= duracionActual) {
     detenerMotores();
     enMovimiento = false;
   }
 }
 
-// Inicia el movimiento según el comando recibido
+// Inicia el movimiento según el comando y ajusta la duración
 void iniciarMovimiento(char comando) {
   switch (comando) {
     case 'w':  // Adelante
@@ -47,27 +48,32 @@ void iniciarMovimiento(char comando) {
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
+      duracionActual = TIEMPO_MOVIMIENTO;
       break;
     case 's':  // Atrás
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
+      duracionActual = TIEMPO_MOVIMIENTO;
       break;
     case 'a':  // Girar izquierda
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
       digitalWrite(IN3, HIGH);
       digitalWrite(IN4, LOW);
+      duracionActual = TIEMPO_GIRO;
       break;
     case 'd':  // Girar derecha
       digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW);
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
+      duracionActual = TIEMPO_GIRO;
       break;
     default:
-      detenerMotores();  // En caso de comando desconocido
+      detenerMotores();
+      duracionActual = 0;
       break;
   }
 }
